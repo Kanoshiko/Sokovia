642df6ad86c6210a83da0594c234c01161d0ae52
Kanoshiko <raidelet@gmail.com>
2016-01-28 08:38:54 +0100

    Amélioration de la partie "compétences".

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,21 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-        controller('CtrPerso', ['$rootScope', '$scope', function ($rootScope, $scope) { }]).
-        controller('CtrType', ['$rootScope', '$scope', function ($rootScope, $scope) { }]).
-        controller('CtrFaction', ['$rootScope', '$scope', function ($rootScope, $scope) { }]).
-        controller('CtrAllegeance', ['$rootScope', '$scope', function ($rootScope, $scope) { }]).
-        controller('CtrSpecialite', ['$rootScope', '$scope', function ($rootScope, $scope) { }]).
-        controller('CtrCompetences', ['$rootScope', '$scope', function ($rootScope, $scope) { }]).
-        controller('CtrPouvoirInhumain', ['$rootScope', '$scope', function ($rootScope, $scope) { }]);
+        controller('CtrPerso', ['$rootScope', '$scope', function ($rootScope, $scope) {
+            }]).
+        controller('CtrType', ['$rootScope', '$scope', function ($rootScope, $scope) {
+            }]).
+        controller('CtrFaction', ['$rootScope', '$scope', function ($rootScope, $scope) {
+            }]).
+        controller('CtrAllegeance', ['$rootScope', '$scope', function ($rootScope, $scope) {
+            }]).
+        controller('CtrSpecialite', ['$rootScope', '$scope', function ($rootScope, $scope) {
+            }]).
+        controller('CtrCompetences', ['$rootScope', '$scope', function ($rootScope, $scope) {
+                $('#myTab a').click(function (e) {
+                    e.preventDefault();
+                    $(this).tab('show');
+                })
+            }]).
+        controller('CtrPouvoirInhumain', ['$rootScope', '$scope', function ($rootScope, $scope) {
+            }]);
